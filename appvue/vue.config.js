87f77bb3581f4a876bff2fee9c64bbdeda5beec1
kirinshibori <80749838+kirinshibori@users.noreply.github.com>
2021-03-20 10:54:27 +0800

    Update vue.config.js

--- appvue/vue.config.js
+++ appvue/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
-	? '/prj3_Dapp_Supply_Chain/'
+	? '/kirinshibori/DrugChainUI'
 	: '/',
   "transpileDependencies": [
     "vuetify"
